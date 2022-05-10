7f6b461a27b9dac478d1ab832e44b3fa8fe30366
Peter Knight <peter.knight@sjsu.edu>
2022-05-10 16:56:23 -0700

    Could it be, do we have a light at the end of this tunnel?

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -158,7 +158,7 @@ public class Client
                                                 + " - retransThreshold " + retransThreshold);
                                         droppedPackets.remove(0);
 
-                                        
+
                                     }
 //                                    This else is for if the retransmission failed again
                                     else{
